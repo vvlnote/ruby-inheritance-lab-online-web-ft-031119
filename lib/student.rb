f3ed01784aa98e68691755a24de087d4f215dd5f
Joy Lin <vvlnote@hotmail.com>
2019-03-28 00:24:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,13 @@ require_relative "./user.rb"
 
 class Student < User
 
+  attr_accessor :knowledge
+  
   def initialize
     @knowledge = []
+  end
+  
+  def learn (str)
+    self.knowledge << str
   end
 end
